011c9db16ce51f3197f435617eacfbeebd56c7e7
Serge Neri <saneri@uncc.edu>
2016-10-29 21:31:42 -0400

    Matthews code working back to master

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,12 @@ router.get('/', function(req, res, next) {
       //request.get('/').query({ ATMList: res.text });
       //res.render(res.body); //do something
       res.render('index', { title: 'Geodeem'});
-
+      router.post('/location');
       //get params for location search
       var params = {
         lat: 35.925797,
         lng: -79.166112,
-        rad: 1
+        rad: 20
       }
       getMerchants(params);
   //});
@@ -36,8 +36,11 @@ var getMerchants = function(params){
   //return resApi;
   });
 }
+router.post('/location', function(req, res, next){
+  console.log(JSON.stringify(req.body) +  "chicken");
+  res.end("okay");
+});
 
-var check
 //request.get('http://api.reimaginebanking.com/atms?lat=38.9283&lng=-77.1753&rad=1&key=14eb8bc8a353786c356e2ab236d923c0').end(function(err, resApi){
 //     //foo(res.status);
 //     //res.render(res.body); //do something
